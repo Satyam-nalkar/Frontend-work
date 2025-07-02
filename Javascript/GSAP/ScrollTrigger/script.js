2f0f554fd5ad20328ae10cb952ed9f9fa5a92f8c
Your Name <your.email@example.com>
2025-07-02 14:48:32 +0530

    pin property

--- Javascript/GSAP/ScrollTrigger/script.js
+++ Javascript/GSAP/ScrollTrigger/script.js
@@ -1,20 +1,36 @@
 gsap.registerPlugin(ScrollTrigger);
-gsap.from("#page1 #circle",{
-    scale:0,
-    delay:1,
-    duration:2,
-    rotate:720
-})
-gsap.from("#page2 #circle",{
-    scale:0, 
-    duration:2,
-    rotate:720,
+// gsap.from("#page1 #circle",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:720
+// })
+// gsap.from("#page2 #circle",{
+//     scale:0, 
+//     duration:2,
+//     rotate:720,
+//     scrollTrigger:{
+//         trigger:"#page2 #circle",
+//         scroller:"body",
+//         markers:true,
+//         start: "top 60%",
+//         end:"top 30%",
+//        scrub:2
+//     } 
+// })
+
+gsap.to("#page2 h1",{
+    transform:"translateX(-120%)" ,
     scrollTrigger:{
-        trigger:"#page2 #circle",
+        trigger:"#page2",
         scroller:"body",
-        markers:true,
-        start: "top 60%",
-        end:"top 30%",
-       scrub:2
-    } 
+        markers:true, 
+        start: "top 0",
+        end:"top -100%",
+        scrub:2,
+        pin:true 
+    }
 })
+
+
+  
