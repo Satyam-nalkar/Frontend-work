49ec350cb76cc7f8a57be3535bdd54e0c34fc78e
Your Name <your.email@example.com>
2025-10-07 21:50:32 +0530

    js datatypes

--- Javascript/Basics/second/sec.js
+++ Javascript/Basics/second/sec.js
@@ -27,5 +27,17 @@
 
 // const discount = 12; 
 //not changeble
+ 
 
+// Window
+//there are many features which are frequently used in js and they are not the part of js, they are not the part of js the language but they are available in the browser when use them in js they called from the browser and not js
+
+
+//Data types
+//harsh = string
+//12 = integer
+//"we234dfej" = string
+
+//12,14.3 , harsh, true,a ,null, undefined - primitive dt
+//[] () {} -reference dt
 
