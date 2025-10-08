2406bd3c837d049329794b710ec150e42b07c47b
Your Name <your.email@example.com>
2025-10-08 14:50:09 +0530

    js for array

--- Javascript/Basics/second/sec.js
+++ Javascript/Basics/second/sec.js
@@ -105,3 +105,100 @@ while(i>0){
     console.log(i);
 }*/
 
+
+//function
+//es5 and es6  
+
+//what is fun -> the part of code whilch is often used in program.
+
+/*function abcd(){ 
+console.log("hello bhai");
+console.log("hello friends");  
+}
+abcd();*/
+
+
+/*var dateofbirth = 7;
+var todaysdate = 7
+function happybirthday() {
+    console.log("Happy Birthday!");
+}
+
+if(dateofbirth === todaysdate){
+    happybirthday();
+} */
+
+
+// how to use 
+// function abcd(){your code}
+
+
+//parameter and arguments
+/*function abcd(val){ //param
+   console.log(12 + val);
+}
+abcd(12);//arguments */
+
+
+/*function abcd(){
+     // function statement
+}
+
+var abcd = function(){
+    //function expression
+}
+
+function(){
+   // anonymous function
+}*/
+
+// fat arrow
+/*var a = ()=>{}; 
+var b = ()=>{};//basic fat arrow */
+
+/*var g = ab =>{}
+g(12)*/
+
+
+//var abcd = () => "harsh";
+    
+
+
+//return
+/*function abcd(){
+    return ;
+}
+// var ans = abcd();
+console.log(abcd());  */
+
+
+//implicit return
+/* var a = () => 12;
+var b = a(); */
+
+//fat arrow
+/* var a = ()=>{
+    return 12;
+}
+var b = a(); */
+
+//undefinrd is a value
+//not defined is an error
+
+
+//null is also a value
+//this is a value which resolve like , not found
+//null is recieved when something is not found
+
+
+//Arrays
+//when you want to store one or many elements collect together.
+//square brackets
+
+/*var users = ["harsh","harshika","harshita","akanksha","nishi"]; 
+console.log(users[2]); */
+
+//we can mix
+//var arr = [1,2,"3",function(){},[],4];
+
+//a lot of times in the shape of more than number ,to contain all data altogether, save it in an array
